e81fc278597a8b21a4c2b9a0fc1598552c0c2135
MisterKindly <9154884415@inbox.ru>
2025-06-09 20:41:20 +0300

    fix tests

--- test/TransactionTest.cpp
+++ test/TransactionTest.cpp
@@ -3,82 +3,54 @@
 #include "../src/Transaction.h"
 
 using ::testing::_;
-using ::testing::AtLeast;
-using ::testing::Invoke;
+using ::testing::Exactly;
+using ::testing::InSequence;
 using ::testing::NiceMock;
 using ::testing::Return;
-using ::testing::Throw;
+using ::testing::StrictMock;
 
-class TransactionTest : public ::testing::Test {
-protected:
-    void SetUp() override {
-	realFrom = new Account(1, 100);
-        realTo = new Account(2, 200);
-        
-        mockFrom = new NiceMock<MockAccount>(1, 100);
-        mockTo = new NiceMock<MockAccount>(2, 200);
-        
-        ON_CALL(*mockFrom, Lock()).WillByDefault(Invoke(realFrom, &Account::Lock));
-        ON_CALL(*mockFrom, Unlock()).WillByDefault(Invoke(realFrom, &Account::Unlock));
-        ON_CALL(*mockFrom, ChangeBalance(_)).WillByDefault(Invoke(realFrom, &Account::ChangeBalance));
-        ON_CALL(*mockFrom, GetBalance()).WillByDefault(Invoke(realFrom, &Account::GetBalance));
-        
-        ON_CALL(*mockTo, Lock()).WillByDefault(Invoke(realTo, &Account::Lock));
-        ON_CALL(*mockTo, Unlock()).WillByDefault(Invoke(realTo, &Account::Unlock));
-        ON_CALL(*mockTo, ChangeBalance(_)).WillByDefault(Invoke(realTo, &Account::ChangeBalance));
-        ON_CALL(*mockTo, GetBalance()).WillByDefault(Invoke(realTo, &Account::GetBalance));
-    }
-    
-    void TearDown() override {
-        delete mockFrom;
-        delete mockTo;
-        delete realFrom;
-        delete realTo;
-    }
-    
-    Account* realFrom;
-    Account* realTo;
-    MockAccount* mockFrom;
-    MockAccount* mockTo;
-};
-
-TEST_F(TransactionTest, MakeInvalidAccounts) {
+TEST(TransactionTest, MakeInvalidAccounts) {
+    testing::StrictMock<MockAccount> acc1(1, 1000);
     Transaction tr;
-    EXPECT_THROW(tr.Make(*mockFrom, *mockFrom, 100), std::logic_error);
+    EXPECT_THROW(tr.Make(acc1, acc1, 100), std::logic_error);
 }
 
-TEST_F(TransactionTest, MakeNegativeSum) {
+TEST(TransactionTest, MakeNegativeSum) {
+    testing::StrictMock<MockAccount> acc1(1, 1000);
+    testing::StrictMock<MockAccount> acc2(2, 2000);
     Transaction tr;
-    EXPECT_THROW(tr.Make(*mockFrom, *mockTo, -50), std::invalid_argument);
+    EXPECT_THROW(tr.Make(acc1, acc2, -50), std::invalid_argument);
 }
 
-TEST_F(TransactionTest, MakeSmallSum) {
+TEST(TransactionTest, MakeSmallSum) {
+    testing::StrictMock<MockAccount> acc1(1, 1000);
+    testing::StrictMock<MockAccount> acc2(2, 2000);
     Transaction tr;
-    EXPECT_THROW(tr.Make(*mockFrom, *mockTo, 99), std::logic_error);
+    EXPECT_THROW(tr.Make(acc1, acc2, 99), std::logic_error);
 }
 
 TEST(TransactionTest, MakeInsufficientFunds) {
-    StrictMock<MockAccount> from(1, 100);
-    StrictMock<MockAccount> to(2, 200);
+    testing::StrictMock<MockAccount> from(1, 100);
+    testing::StrictMock<MockAccount> to(2, 200);
     Transaction tr;
 
     ON_CALL(from, GetBalance()).WillByDefault(Return(100));
     ON_CALL(to, GetBalance()).WillByDefault(Return(200));
 
     {
-        InSequence seq;
-        EXPECT_CALL(from, Lock()).Times(Exactly(1));
-        EXPECT_CALL(from, GetBalance()).Times(Exactly(1));
-        EXPECT_CALL(from, Unlock()).Times(Exactly(1));
+        testing::InSequence seq;
+        EXPECT_CALL(from, Lock()).Times(testing::Exactly(1));
+        EXPECT_CALL(from, GetBalance()).Times(testing::Exactly(1));
+        EXPECT_CALL(from, Unlock()).Times(testing::Exactly(1));
     }
 
     {
-        InSequence seq;
-        EXPECT_CALL(to, Lock()).Times(Exactly(1));
-        EXPECT_CALL(to, ChangeBalance(300)).Times(Exactly(1));  // Зачисление
-        EXPECT_CALL(to, ChangeBalance(-300)).Times(Exactly(1)); // Откат
-        EXPECT_CALL(to, GetBalance()).Times(Exactly(1));        // Для SaveToDataBase
-        EXPECT_CALL(to, Unlock()).Times(Exactly(1));
+        testing::InSequence seq;
+        EXPECT_CALL(to, Lock()).Times(testing::Exactly(1));
+        EXPECT_CALL(to, ChangeBalance(300)).Times(testing::Exactly(1));  
+        EXPECT_CALL(to, ChangeBalance(-300)).Times(testing::Exactly(1)); 
+        EXPECT_CALL(to, GetBalance()).Times(testing::Exactly(1));        
+        EXPECT_CALL(to, Unlock()).Times(testing::Exactly(1));
     }
 
     EXPECT_CALL(from, ChangeBalance(_)).Times(0);
@@ -87,28 +59,28 @@ TEST(TransactionTest, MakeInsufficientFunds) {
 }
 
 TEST(TransactionTest, MakeSuccess) {
-    StrictMock<MockAccount> from(1, 1000);
-    StrictMock<MockAccount> to(2, 2000);
+    testing::StrictMock<MockAccount> from(1, 1000);
+    testing::StrictMock<MockAccount> to(2, 2000);
     Transaction tr;
 
     ON_CALL(from, GetBalance()).WillByDefault(Return(1000));
     ON_CALL(to, GetBalance()).WillByDefault(Return(2000));
 
     {
-        InSequence seq;
-        EXPECT_CALL(from, Lock()).Times(Exactly(1));
-        EXPECT_CALL(from, GetBalance()).Times(Exactly(1));
-        EXPECT_CALL(from, ChangeBalance(-301)).Times(Exactly(1)); 
-        EXPECT_CALL(from, GetBalance()).Times(Exactly(1));       
-        EXPECT_CALL(from, Unlock()).Times(Exactly(1));
+        testing::InSequence seq;
+        EXPECT_CALL(from, Lock()).Times(testing::Exactly(1));
+        EXPECT_CALL(from, GetBalance()).Times(testing::Exactly(1));
+        EXPECT_CALL(from, ChangeBalance(-301)).Times(testing::Exactly(1)); 
+        EXPECT_CALL(from, GetBalance()).Times(testing::Exactly(1));        
+        EXPECT_CALL(from, Unlock()).Times(testing::Exactly(1));
     }
 
     {
-        InSequence seq;
-        EXPECT_CALL(to, Lock()).Times(Exactly(1));
-        EXPECT_CALL(to, ChangeBalance(300)).Times(Exactly(1)); 
-        EXPECT_CALL(to, GetBalance()).Times(Exactly(1));       
-        EXPECT_CALL(to, Unlock()).Times(Exactly(1));
+        testing::InSequence seq;
+        EXPECT_CALL(to, Lock()).Times(testing::Exactly(1));
+        EXPECT_CALL(to, ChangeBalance(300)).Times(testing::Exactly(1)); 
+        EXPECT_CALL(to, GetBalance()).Times(testing::Exactly(1));       
+        EXPECT_CALL(to, Unlock()).Times(testing::Exactly(1));
     }
 
     EXPECT_TRUE(tr.Make(from, to, 300));
