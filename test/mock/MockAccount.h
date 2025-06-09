dae39b82aff94ecc9fcae3983b9ff743d5a5a197
MisterKindly <9154884415@inbox.ru>
2025-06-09 23:33:25 +0300

    fixed mockacc

--- test/mock/MockAccount.h
+++ test/mock/MockAccount.h
@@ -4,27 +4,9 @@
 
 class MockAccount : public Account {
 public:
-    MockAccount(int id, int balance);
-    virtual ~MockAccount();
-
-    int GetBalance() const override { 
-        return GetBalanceMock(); 
-    }
-    
-    void ChangeBalance(int diff) override { 
-        ChangeBalanceMock(diff); 
-    }
-    
-    void Lock() override { 
-        LockMock(); 
-    }
-    
-    void Unlock() override { 
-        UnlockMock(); 
-    }
-
-    MOCK_METHOD(int, GetBalanceMock, (), (const));
-    MOCK_METHOD(void, ChangeBalanceMock, (int diff));
-    MOCK_METHOD(void, LockMock, ());
-    MOCK_METHOD(void, UnlockMock, ());
+    MockAccount(int id, int balance): Account(id, balance){}
+    MOCK_METHOD(int, GetBalance, (), (const, override));
+    MOCK_METHOD(void, ChangeBalance, (int diff), (override));
+    MOCK_METHOD(void, Lock, (), (override));
+    MOCK_METHOD(void, Unlock, (), (override));
 };
