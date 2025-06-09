1070df6765e8523081b2fc1b706cdbb37c98ec18
MisterKindly <9154884415@inbox.ru>
2025-06-09 13:06:43 +0300

    fix tests

--- test/TransactionTest.cpp
+++ test/TransactionTest.cpp
@@ -3,6 +3,7 @@
 #include "../src/Transaction.h"
 
 using ::testing::_;
+using ::testing::InSequence;
 using ::testing::NiceMock;
 using ::testing::Return;
 using ::testing::Throw;
@@ -30,32 +31,52 @@ TEST(TransactionTest, MakeSmallSum) {
 TEST(TransactionTest, MakeInsufficientFunds) {
     MockAccount from(1, 100);
     MockAccount to(2, 200);
-
-    EXPECT_CALL(from, Lock()).Times(1);
-    EXPECT_CALL(to, Lock()).Times(1);
-    EXPECT_CALL(to, ChangeBalance(300)).Times(1);
-    EXPECT_CALL(from, GetBalance()).WillOnce(Return(100));
-    EXPECT_CALL(from, ChangeBalance(302)).Times(0);
-    EXPECT_CALL(to, ChangeBalance(-300)).Times(1);
-    EXPECT_CALL(from, Unlock()).Times(1);
-    EXPECT_CALL(to, Unlock()).Times(1);
-
     Transaction tr;
+
+    // Для from
+    {
+        InSequence s;
+        EXPECT_CALL(from, Lock()).Times(1);
+        // GetBalance вызывается внутри Debit()
+        EXPECT_CALL(from, GetBalance()).WillOnce(Return(100));
+        // ChangeBalance не вызывается, так как средств недостаточно
+        EXPECT_CALL(from, Unlock()).Times(1);
+    }
+
+    // Для to
+    {
+        InSequence s;
+        EXPECT_CALL(to, Lock()).Times(1);
+        EXPECT_CALL(to, ChangeBalance(300)).Times(1);  // Зачисление
+        EXPECT_CALL(to, ChangeBalance(-300)).Times(1); // Откат из-за недостатка средств
+        EXPECT_CALL(to, Unlock()).Times(1);
+    }
+
     EXPECT_FALSE(tr.Make(from, to, 300));
 }
 
 TEST(TransactionTest, MakeSuccess) {
     MockAccount from(1, 1000);
     MockAccount to(2, 2000);
-
-    EXPECT_CALL(from, Lock()).Times(1);
-    EXPECT_CALL(to, Lock()).Times(1);
-    EXPECT_CALL(to, ChangeBalance(300)).Times(1);
-    EXPECT_CALL(from, GetBalance()).WillOnce(Return(1000));
-    EXPECT_CALL(from, ChangeBalance(-302)).Times(1);
-    EXPECT_CALL(to, Unlock()).Times(1);
-    EXPECT_CALL(from, Unlock()).Times(1);
-
     Transaction tr;
+
+    // Для from
+    {
+        InSequence s;
+        EXPECT_CALL(from, Lock()).Times(1);
+        // GetBalance вызывается внутри Debit()
+        EXPECT_CALL(from, GetBalance()).WillOnce(Return(1000));
+        EXPECT_CALL(from, ChangeBalance(-301)).Times(1); // 300 + комиссия 1
+        EXPECT_CALL(from, Unlock()).Times(1);
+    }
+
+    // Для to
+    {
+        InSequence s;
+        EXPECT_CALL(to, Lock()).Times(1);
+        EXPECT_CALL(to, ChangeBalance(300)).Times(1); // Зачисление
+        EXPECT_CALL(to, Unlock()).Times(1);
+    }
+
     EXPECT_TRUE(tr.Make(from, to, 300));
 }
